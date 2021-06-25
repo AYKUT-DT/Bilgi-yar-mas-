4e1dab0f960be1413ab088ce8be4ad5c136524e0
AYKUT-DT <75445965+AYKUT-DT@users.noreply.github.com>
2021-06-25 10:02:12 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const dakika = document.getElementById("dk");
 let saniye = document.getElementById("sn");
 
 let pay = document.getElementById("pay");
+let payda = document.getElementById("payda");
 
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
@@ -12,9 +13,11 @@ let msgText = document.getElementById("msg-text");
 let interval;
 let title = document.getElementById('title');
 let rule = document.getElementById('rule');
+let score = document.getElementById('score');
+let km = document.getElementById('km');
+let joker = document.getElementById('joker');
 
 let shuffledQuestions, currentQuestionIndex;
-
 
 startButton.addEventListener('click', startGame)
 startButton.addEventListener('click', startTimer)
@@ -55,17 +58,29 @@ function stopTimer() {
 
 
 
+score.style.display = "none";
+km.style.display = "none";
+joker.style.display = "none";
+joker.addEventListener('click', () => {
+  eliminate();
+});
+
+
 
 function startGame() {
+  msgText.innerText = "";
+  score.style.display = "block";
+  km.style.display = "block";
+  joker.style.display = "block";
   title.style.display = "none";
   rule.style.display = "block";
   setTimeout(function () {
     rule.style.display = "none";
-  }, 3000)
+  }, 2000)
 
   pay.textContent = 0;
   startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - 1)
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
   currentQuestionIndex = 0
   questionContainerElement.classList.remove('hide')
   setNextQuestion()
@@ -77,9 +92,9 @@ function setNextQuestion() {
 }
 
 function showQuestion(question, questionIndex) {
-  questionElement.innerText = question.question
+  questionElement.innerText = question.question;
   question.answers.forEach((answer, index) => {
-    button = document.createElement('button')
+    button = document.createElement('button');
     button.innerText = answer.text;
     button.classList.add('btn')
     button.setAttribute("data-question-index", currentQuestionIndex);
@@ -100,9 +115,9 @@ function resetState() {
 }
 
 
+
 function selectAnswer(e) {
   stopTimer();
-
   var questionIndex = parseInt(e.target.getAttribute("data-question-index"), 10);
   var optionIndex = parseInt(e.target.getAttribute("data-options-index"), 10);
   var answer = shuffledQuestions[questionIndex].answers[optionIndex];
@@ -115,14 +130,50 @@ function selectAnswer(e) {
 
     setTimeout(function () {
       if (answer.correct) {
-        e.target.classList.add('correct');
-        let pay1 = pay.textContent;
-        pay1++;
-        pay.textContent = pay1;
+        const audio = document.getElementById('audio1');
+        audio.volume = 0.2;
+        audio.play();
+
+        setTimeout(function () {
+          e.target.classList.add('correct');
+          let pay1 = pay.textContent;
+          pay1++;
+          pay.textContent = pay1;
+          if (shuffledQuestions.length > currentQuestionIndex + 1) {
+            nextButton.classList.remove('hide');
+          } else {
+            control();
+          }
+
+
+        }, 500)
+
+
+
+
+        if (shuffledQuestions.length == currentQuestionIndex + 1) {
+          control();
+        }
       }
       else {
-        e.target.classList.add('wrong');
-        showAnswer1();
+        const audioWrong = document.getElementById('audio2');
+        audioWrong.volume = 0.2;
+        audioWrong.play();
+        setTimeout(function () {
+          e.target.classList.add('wrong');
+          showAnswer1();
+          if (shuffledQuestions.length > currentQuestionIndex + 1) {
+            nextButton.classList.remove('hide');
+          } else {
+            control();
+          }
+        }, 1000)
+
+
+        if (shuffledQuestions.length == currentQuestionIndex + 1) {
+          control();
+        }
+
       }
 
     }, 3000)
@@ -130,17 +181,30 @@ function selectAnswer(e) {
 
   }
 
+  function control() {
+    nextButton.classList.add('hide')
+    if (shuffledQuestions.length == currentQuestionIndex + 1) {
+      startButton.innerText = 'Yeniden Oyna'
+      startButton.classList.remove('hide')
+      let message = 'OYUN BİTTİ';
+      flash_msg(message);
+    }
 
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
-  } else {
-    startButton.innerText = 'Yeniden Oyna'
-    startButton.classList.remove('hide')
-    let message = 'OYUN BİTTİ';
-    flash_msg(message);
+
   }
 
 
+
+
+}
+
+function eliminate(e) {
+  var questionIndex = parseInt(e.target.getAttribute("data-question-index"), 10);
+  var optionIndex = parseInt(e.target.getAttribute("data-options-index"), 10);
+  var answer = shuffledQuestions[questionIndex].answers[optionIndex];
+  if (answer.correct === false) {
+    answerButtonsElement.removeChild(answerButtonsElement.answer);
+  }
 
 }
 
@@ -200,42 +264,42 @@ function flash_msg(message) {
   msgText.innerText = message;
   setTimeout(function () {
     msgText.innerText = 'Sonuç: ' + pay.textContent + '/4';
-  }, 1000);
+  }, 2000);
 }
 
 
 const questions = [
   {
-    question: 'Yeni Zelandanın yerli halkına ne ad verilir?',
+    question: '24 saat içinde müşteri için azami kazanç ne kadardır?',
     answers: [
-      { text: 'A: Maoriler', correct: true },
-      { text: 'B: Aborjinler', correct: false }
+      { text: 'A: 100.000 Euro', correct: true },
+      { text: 'B: 50.000 Euro', correct: false }
     ]
   },
   {
-    question: 'İstiklal Şairi olarak anılan şair aşağıdakilerden hangisidir?',
+    question: 'Bet üyelerinin belge kontrol süreleri ne kadardır?',
     answers: [
-      { text: 'Yahya Kemal Beyatlı', correct: false },
-      { text: 'Ziya Gökalp', correct: false },
-      { text: 'Mehmed Akif Ersoy', correct: true },
-      { text: 'Yakub Kadri Karaosmanoğlu', correct: false }
+      { text: 'A: 12 saat', correct: false },
+      { text: 'B: 16 saat', correct: false },
+      { text: 'C: 48 saat', correct: true },
+      { text: 'D: 72 saat', correct: false }
     ]
   },
   {
 
-    question: 'Kızınca tüküren hayvan hangisidir?',
+    question: 'Express veya sistem bahisleri için max oran nedir?',
     answers: [
-      { text: 'Panda', correct: false },
-      { text: 'Lama', correct: true }
+      { text: 'A: 500', correct: false },
+      { text: 'B: 1000', correct: true }
     ]
   },
   {
-    question: 'Filmlerde dedektiflerin sıkça kullandıkları alet hangisidir?',
+    question: 'Futbolcuyken, Türkiyede en üst ligde gol kralı olan teknik direktör hangisidir?',
     answers: [
-      { text: 'A: Teleskop', correct: false },
-      { text: 'B: Büyüteç', correct: true },
-      { text: 'C: Mikroskop', correct: false },
-      { text: 'D: Gözlük', correct: false }
+      { text: 'A: Fatih Terim', correct: false },
+      { text: 'B: Mustafa Denizli', correct: true },
+      { text: 'C: Ersun Yanal', correct: false },
+      { text: 'D: Şenol Güneş', correct: false }
     ]
   }
 ]
